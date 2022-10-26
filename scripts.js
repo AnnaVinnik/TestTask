b02224b90440731b2cc259eb0f0e1a1c905f4faa
Anna <vinnik.anna@rambler.ru>
2022-10-26 22:50:22 +0700

    Chenged hiding elements

--- scripts.js
+++ scripts.js
@@ -4,19 +4,21 @@ let cards = [];
 
 const cardsOnPage = document.querySelectorAll(".custom-card");
 
-for(let i = 0; i < cardsOnPage.length; i++) {                //Создаем массив объектов со всеми карточками на странице
-    cards.push({
-        id: i,
-        departuresList: cardsOnPage[i].querySelectorAll(".custom-card__departures_time"),
-        departures: cardsOnPage[i].querySelector(".custom-card__departures"),
-        button: document.createElement("div"),
-    });
-    cards[i].elementsToHide = Array.from(cards[i].departuresList).slice(3, cards[i].departuresList.length);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    for(let i = 0; i < cardsOnPage.length; i++) {                //Создаем массив объектов со всеми карточками на странице
+        cards.push({
+            id: i,
+            departuresList: cardsOnPage[i].querySelectorAll(".custom-card__departures_time"),
+            departures: cardsOnPage[i].querySelector(".custom-card__departures"),
+            button: document.createElement("div"),
+        });
+        cards[i].elementsToHide = Array.from(cards[i].departuresList).slice(3, cards[i].departuresList.length);
+    }
 
-for(let i = 0; i < cardsOnPage.length; i++) {                 //Вызываем функцию скрытия элементов для всех карточек
-    hideElements(i);
-}
+    for(let i = 0; i < cardsOnPage.length; i++) {                 //Вызываем функцию скрытия элементов для всех карточек
+        hideElements(i);
+    }
+});
 
 function hideElements(id) {       //Скрытие элементов, которые не входят в строку
     if (cards[id].departuresList.length > 4) {
