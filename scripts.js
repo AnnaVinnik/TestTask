a60c8eb0f09095b7a6e2f2e5515fedb68db9648e
Anna <vinnik.anna@rambler.ru>
2022-10-26 21:12:12 +0700

    Added comments

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@
 
 const departuresList = document.querySelectorAll(".custom-card__departures_time");
 const departures = document.querySelector(".custom-card__departures");
-const elementsToHide = Array.from(departuresList).slice(3, departuresList.length);
+const elementsToHide = Array.from(departuresList).slice(3, departuresList.length);    //Элементы, которые не входят в строку
 const button = document.createElement("div");
 
 hideElements();
 
-function hideElements() {
+function hideElements() {       //Скрытие элементов, которые не входят в строку
     if (departuresList.length > 4) {
         elementsToHide.forEach(elem => elem.classList.add("d-none"));
 
@@ -19,7 +19,7 @@ function hideElements() {
     }
 }
 
-function showElements() {
+function showElements() {           //Отображение элементов, ктоторые не входят в строку
     elementsToHide.forEach(elem => elem.classList.remove("d-none"));
     button.classList.add("d-none");
 }
